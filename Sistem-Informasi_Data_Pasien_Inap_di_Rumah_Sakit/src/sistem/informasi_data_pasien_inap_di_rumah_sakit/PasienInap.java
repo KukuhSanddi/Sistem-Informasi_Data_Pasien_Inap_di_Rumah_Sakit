8677f69a8d89a0bac454bfd0279221d05dafc4d2
nisanurhardini <prihardinii@gmail.com>
2017-03-16 21:47:46 +0700

    update PasienInap.java
    update 1

--- Sistem-Informasi_Data_Pasien_Inap_di_Rumah_Sakit/src/sistem/informasi_data_pasien_inap_di_rumah_sakit/PasienInap.java
+++ Sistem-Informasi_Data_Pasien_Inap_di_Rumah_Sakit/src/sistem/informasi_data_pasien_inap_di_rumah_sakit/PasienInap.java
@@ -10,5 +10,19 @@ package sistem.informasi_data_pasien_inap_di_rumah_sakit;
  * @author Kukuh Sanddi
  */
 public class PasienInap {
+    private Pasien pasien;
+    private Dokter dokter;
+    private String[] diagnosa;
     
+    public void setDokter (Dokter d){
+    
+    }
+    
+    public void addDiagnosa (String d){
+    
+    }
+    
+    public String getDiagnosa (int a){
+        return diagnosa[a];
+    }
 }
